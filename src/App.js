4f748de361eaee25287d9a3bcfb5b056d724e9b1
Leonardo Luiz <leonardokl@hotmail.com>
2020-04-01 23:37:46 -0300

    feat: add characters list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { request } from "./utils";
+
+function usePeople() {
+  const [status, setStatus] = useState("loading");
+  const [response, setResponse] = useState({ results: [] });
+  async function load() {
+    setStatus("loading");
+
+    try {
+      const data = await request(
+        response.next || "https://swapi.co/api/people/"
+      );
+
+      setResponse({
+        ...data,
+        results: response.results.concat(data.results)
+      });
+      setStatus("resolved");
+    } catch (ex) {
+      console.error(ex);
+      setStatus("error");
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return { status, response, fetchMore: load };
+}
 
 function App() {
+  const {
+    response: { results, next },
+    fetchMore,
+    status
+  } = usePeople();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="App">
+      <ul aria-label="Characters" className="characters">
+        {results.map(person => (
+          <li key={person.id} className="characters-item">
+            {person.name}
+          </li>
+        ))}
+      </ul>
+      {status === "loading" && <div>Loading...</div>}
+      {status !== "loading" && !!next && (
+        <button
+          className="button"
+          onClick={fetchMore}
+          disabled={status === "loading"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          SHOW MORE
+        </button>
+      )}
+    </main>
   );
 }
 
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export function request(endpoint) {
+  return window.fetch(endpoint).then(async response => {
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
+}
